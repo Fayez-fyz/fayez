9ab58b37eb3e7e0cf7a96bff2a25e50fb3884c70
Fayez-fyz <77930171+Fayez-fyz@users.noreply.github.com>
2021-06-16 21:08:31 +0530

    Add files via upload

---
+++ typeof.js
@@ -0,0 +1,21 @@
+let a = 1;
+let b = 1.1;
+let c = '1.1';
+let d = true;
+let e = null;
+let f;
+let g= [a,b,c];
+let h = {a:1,b:2,c:3};
+let i = NaN;
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+console.log(typeof(g));
+console.log(typeof(h));
+console.log(typeof(i));
+
+
